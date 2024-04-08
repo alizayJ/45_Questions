a2cc55428502156fd0d6507bccd594c4e3f2b332
alizayJ <116749097+alizayJ@users.noreply.github.com>
2024-04-08 18:40:43 +0500

    Add files via upload

---
+++ Question_8/Question_8.js
@@ -0,0 +1,8 @@
+// You should create four lines that look like this:
+//console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+//The solution of this will be same as Question_7
+console.log(4 + 4);
+console.log(2 * 4);
+console.log(12 - 4);
+console.log(64 / 8);
