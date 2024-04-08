e00289792024da2b223eed09b856517a05307f7f
alizayJ <116749097+alizayJ@users.noreply.github.com>
2024-04-08 17:41:02 +0500

    Add files via upload

---
+++ Question_4/Question_4.ts
@@ -0,0 +1,5 @@
+// Famous Quote: Find a quote from a famous person you admire. 
+//Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+console.log(`An Anonymous person once said, "The first step to getting anywhere is deciding you’re no longer willing to stay where you are"`)
---
+++ Question_5/Question_5.js
@@ -0,0 +1,5 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+// Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "anonymous";
+var message = "An ".concat(famous_person, " person once said, \"The first step to getting anywhere is deciding you\u2019re no longer willing to stay where you are\"");
+console.log(message);
