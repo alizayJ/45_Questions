69459db590ffb0cafb2aca90f95449c1a1429739
alizayJ <116749097+alizayJ@users.noreply.github.com>
2024-04-08 20:16:28 +0500

    Add files via upload

---
+++ Question_9/Question_9.js
@@ -0,0 +1,5 @@
+//Favorite Number: Store your favorite number in a variable. 
+//Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favorite_number = 1;
+var message = "My favorite number is: ".concat(favorite_number);
+console.log(message);
---
+++ Question_9/Question_9.ts
@@ -0,0 +1,6 @@
+//Favorite Number: Store your favorite number in a variable. 
+//Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favorite_number = 1 ;
+let message = `My favorite number is: ${favorite_number}`;
+console.log(message);
