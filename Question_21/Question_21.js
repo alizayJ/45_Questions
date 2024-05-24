c9bbac9d6d96b96ebdb05b198d3916e5aef0618b
alizayJ <116749097+alizayJ@users.noreply.github.com>
2024-05-24 20:30:32 +0500

    Add files via upload

---
+++ Question_21/Question_21.js
@@ -0,0 +1,11 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var person = {
+    Name: "Emily",
+    Lastname: "Lavender",
+    Age: 30,
+    isEmployed: true,
+};
+console.log(person.Name);
+console.log(person.Lastname);
+console.log(person.Age);
+console.log(person.isEmployed);
---
+++ Question_21/Question_21.ts
@@ -0,0 +1,12 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+const person = {
+    Name : "Emily",
+    Lastname : "Lavender",
+    Age : 30,
+    isEmployed : true,
+}
+console.log(person.Name);
+console.log(person.Lastname);
+console.log(person.Age);
+console.log(person.isEmployed);
