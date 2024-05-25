c0848393d847a8e33adc406e4d4f136315eb90a0
alizayJ <116749097+alizayJ@users.noreply.github.com>
2024-05-25 14:54:42 +0500

    Add files via upload

---
+++ Question_25/Question_25.js
@@ -0,0 +1,26 @@
+//Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 
+//'green', 'yellow', or 'red'.
+//• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+//• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+//
+var alien_color_0 = 'red';
+if (alien_color_0 === 'green') {
+    console.log('you just earn yourself 5 points');
+}
+else {
+    console.log("you didn't earn any points");
+}
+var alien_color_1 = 'yellow';
+if (alien_color_1 === 'yellow') {
+    console.log('if test passed');
+}
+else if (alien_color_1 === 'red') {
+    console.log(null);
+}
+var alien_color_2 = 'red';
+if (alien_color_2 === 'yellow') {
+    console.log('if test passed');
+}
+else if (alien_color_2 === 'red') {
+    console.log(null);
+}
---
+++ Question_27/Question_27.js
@@ -0,0 +1,35 @@
+//Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+//• If the alien is green, print a message that the player earned 5 points.
+//• If the alien is yellow, print a message that the player earned 10 points.
+//• If the alien is red, print a message that the player earned 15 points.
+//• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+var Alien_color_1 = 'green';
+if (Alien_color_1 === 'green') {
+    console.log('you just earn yourself 5 points');
+}
+else if (Alien_color_1 === 'yellow') {
+    console.log('you just earn yourself 10 points');
+}
+else if (Alien_color_1 === 'red') {
+    console.log('you just earn yourself 15 points');
+}
+var Alien_color_2 = 'yellow';
+if (Alien_color_2 === 'green') {
+    console.log('you just earn yourself 5 points');
+}
+else if (Alien_color_2 === 'yellow') {
+    console.log('you just earn yourself 10 points');
+}
+else if (Alien_color_2 === 'red') {
+    console.log('you just earn yourself 15 points');
+}
+var Alien_color_3 = 'red';
+if (Alien_color_3 === 'green') {
+    console.log('you just earn yourself 5 points');
+}
+else if (Alien_color_3 === 'yellow') {
+    console.log('you just earn yourself 10 points');
+}
+else if (Alien_color_3 === 'red') {
+    console.log('you just earn yourself 15 points');
+}
