c0a3cf689361abbee94b5ee3abca22e8092d9546
alizayJ <116749097+alizayJ@users.noreply.github.com>
2024-06-02 18:19:01 +0500

    Add files via upload

---
+++ Question_36/Question_36.ts
@@ -0,0 +1,7 @@
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt.
+// The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+function make_shirt(sizeinch:number,msg:string){
+    console.log(`The size of shirt is ${sizeinch} inch,\nand the message to be printed is "${msg}"`)
+}
+make_shirt(20,"Hello");
