6ef1bf7d987bee06091c3b6f7bb0675d62af8961
alizayJ <116749097+alizayJ@users.noreply.github.com>
2024-06-02 18:09:16 +0500

    Add files via upload

---
+++ Question_35/Question_35.js
@@ -0,0 +1,16 @@
+//Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, 
+//and then use a for loop to print out the name of each animal.
+// • Modify your program to print a statement about each animal, such as A dog would make a great pet. 
+//• Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals 
+//would make a great pet!
+var wildanimals = ["Lion", "Deer", "Panther"];
+for (var i = 0; i < wildanimals.length; i++) {
+    console.log(wildanimals[i]);
+}
+// modified
+for (var _i = 0, wildanimals_1 = wildanimals; _i < wildanimals_1.length; _i++) {
+    var wild = wildanimals_1[_i];
+    console.log(wild, "is a wild animal.");
+}
+// last sentence 
+console.log("The mentioned animals are Wild animals. They live in forest and two of them are dangerous for human beings namely Lion and Panther");
