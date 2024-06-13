2dd2965eae8e4fee2f2c4ad1852502901691b247
alizayJ <116749097+alizayJ@users.noreply.github.com>
2024-06-14 03:27:36 +0500

    Add files via upload

---
+++ Question_42/Question_42.ts
@@ -0,0 +1,11 @@
+//Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of
+// magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+
+let magician_name = ["David Blaine", "David Copperfield", "Dynamo", "Lance Burton", "David Nixon"]
+function make_great(magician: string[]){
+    for(let i = 0; i < magician.length; i++){
+     magician[i] = "the great " + magician[i]
+    };
+    return magician;
+}
+console.log(make_great(magician_name));
---
+++ Question_43/Question_43.ts
@@ -0,0 +1,25 @@
+//Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. 
+//Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians()
+// with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+
+
+const magician_name = ["David Blaine", "David Copperfield", "Dynamo", "Lance Burton", "David Nixon"]
+// Modified Array
+function make_great(magician: string[]){
+    let new_magician: string[] = [];
+    for(let i = 0; i < magician.length; i++){
+     new_magician.push("the great " + magician[i])
+    };
+    return new_magician;
+}
+// Original Array
+function show_magicians(magician: string[]): void{
+    //for(let i = 0; i < magician.length; i++){
+        console.log(magician)
+    //}
+}
+
+console.log("Original Array ");
+show_magicians(magician_name)
+console.log("Modified Array");
+console.log(make_great(magician_name));
